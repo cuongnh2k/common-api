b90736e32af698c18cd6a516309a05400d90b661
cuongnh2k <cuongnh2k@gmail.com>
2023-03-31 23:51:27 +0700

    create project

---
+++ src/test/java/space/cuongnh2k/CommonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package space.cuongnh2k;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
